b2128184f26486dcddec2a268d6f96c4dae3b3d5
YesImDexter <116723600+YesImDexter@users.noreply.github.com>
2024-02-24 15:48:31 +0800

    Completed and Fully Functioning
    Needs Future Updates Tho

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-const btnSearch = document.getElementById("button-search");
 const inputSearch = document.getElementById("input-search");
+const btnSearch = document.getElementById("button-search");
 
 const root = document.documentElement;
 
-//Get the toggle button
+document.getElementById("info").style.display = "none";
+document.getElementById("prompt").style.display = "none";
 
+//Get the toggle button
 const toggle = document.getElementById("toggle");
 
 // Get the user's preference from localStorage
@@ -26,3 +28,55 @@ toggle.addEventListener("click", () => {
     localStorage.removeItem("dark-mode");
   }
 });
+
+// Function to handle "Enter" key press
+inputSearch.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    // Call getWeather() function when "Enter" key is pressed
+    getWeather();
+  }
+});
+
+// Function to get weather data
+function getWeather() {
+
+  document.getElementById("info").style.display = "none";
+  document.getElementById("prompt").style.display = "none";
+
+  //LMAO IM TOO LAZY TO HIDE MY API KEY< PLEASE DONT STEAL IT
+  const APIToken = "5b6ddf2fc7a4a520ef84c3317aaf5e1c";
+
+  const apiUrl = "http://api.openweathermap.org/data/2.5/weather?q="+inputSearch.value+"&units=metric"+"&appid="+APIToken;
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network Error");
+      }
+
+      //Parse JSON data
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+
+      document.getElementById("info").style.display = "flex";
+
+      //clear, clouds, drizzle, mist, rain, snow
+
+      document.getElementById("name").innerText = data.name;
+      document.getElementById("temp").innerText = data.main.temp;
+      document.getElementById("humidity").innerText = data.main.humidity;
+      document.getElementById("wind").innerText = data.wind.speed;
+      document.getElementById("desc").innerText = data.weather[0].description;
+      document.getElementById("weather").innerText = data.weather[0].main;
+    })
+    .catch(error => {
+      //Handle Errors
+      console.error("There was a problem with the fetch operation");
+
+      document.getElementById("prompt").style.display = "flex";
+    })
+}
+// 2.5/forecast?id=524901&appid={API key}
